11eeec6845846c84cd4a165659bf147e7ec72fbf
vidalmiakoleen-create <vidalmiakoleen@gmail.com>
2025-11-09 15:21:41 +0800

    Update Inheritance act.java

--- Inheritance act.java
+++ Inheritance act.java
@@ -1,36 +1,60 @@
 class Person {
-    void name() { System.out.println("Name"); }
+    protected String name;
+
+    void showInfo() {
+        System.out.println("Name: " + name);
+    }
 }
+
+class Student extends Person {
     
-class Student extends Person{
-    void showInfo(String name) {
+    void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    void showInfo() {
         System.out.println("I am " + name + ", a student.");
     }
+
     void sendMessage() {
-        System.out.println("Message sent to teacher");
+        System.out.println("Message sent to teacher.");
+    }
+
+    void sendMessage(String msg) {
+        System.out.println("Message sent to teacher: " + msg);
     }
 }
-class Teacher extends Person{
-    void showInfo(String name) {
+
+class Teacher extends Person {
+    
+    void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    void showInfo() {
         System.out.println("I am " + name + ", a teacher.");
     }
-    void sendMessage(String msg){
-        System.out.println("Message sent a teacher: " + msg);
+
+    void sendMessage(String msg) {
+        System.out.println("Message sent to teacher: " + msg);
     }
 }
 
 public class Main {
-    public static void main(String[] args){
-        Student S= new Student();
-        Teacher T= new Teacher();
-        
-        S.name();
-        S.showInfo("Mia");
+    public static void main(String[] args) {
+        Student S = new Student();
+        Teacher T = new Teacher();
+
+        S.setName("Mia");
+        T.setName("Jasmin");
+
+        S.showInfo();
+        T.showInfo();
+
         S.sendMessage();
-        
-        T.name();
-        T.showInfo("Jasmin");
-        T.sendMessage("Pass your Activities before 10 o'clock");
+        S.sendMessage("Good day ma'am!");
+
     }
 }
-
