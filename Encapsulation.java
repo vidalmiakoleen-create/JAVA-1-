3adae6f4a21bd641baaa04c0b9ec66b1be7b9884
vidalmiakoleen-create <vidalmiakoleen@gmail.com>
2025-11-17 16:27:27 +0800

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,19 +2,16 @@ class BankAccount {
     private String owner;
     private double balance;
 
-    // Constructor
     public BankAccount(String owner, double balance) {
         this.owner = owner;
         this.balance = balance;
     }
 
-    // Deposit method
     public void deposit(double amount) {
         balance += amount;
         System.out.println(owner + " deposited Php " + amount + ". New balance: Php " + balance);
     }
 
-    // Withdraw method
     public void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
@@ -24,7 +21,6 @@ class BankAccount {
         }
     }
 
-    // Main method to test
     public static void main(String[] args) {
         BankAccount amount1 = new BankAccount("Mia", 100000);
         amount1.deposit(1500);
