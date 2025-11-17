218c84c173fd216f8d144358866562ba0231c4e2
vidalmiakoleen-create <vidalmiakoleen@gmail.com>
2025-11-17 15:49:37 +0800

    Inheritance act.java

--- Inheritance act.java
+++ Inheritance act.java
@@ -1,60 +1,37 @@
-class Person {
-    protected String name;
+class Vehicle {
+    String brand;
 
-    void showInfo() {
-        System.out.println("Name: " + name);
+    void start() {
+        System.out.println("Vehicle is starting...");
     }
 }
 
-class Student extends Person {
-    
-    void setName(String name) {
-        this.name = name;
-    }
+class Car extends Vehicle {
+    String model;
+    String color;
+    String gasoline;
+    String year_released;
 
-    @Override
-    void showInfo() {
-        System.out.println("I am " + name + ", a student.");
-    }
-
-    void sendMessage() {
-        System.out.println("Message sent to teacher.");
-    }
-
-    void sendMessage(String msg) {
-        System.out.println("Message sent to teacher: " + msg);
-    }
-}
-
-class Teacher extends Person {
-    
-    void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    void showInfo() {
-        System.out.println("I am " + name + ", a teacher.");
-    }
-
-    void sendMessage(String msg) {
-        System.out.println("Message sent to teacher: " + msg);
+    void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Gasoline: " + gasoline);
+        System.out.println("Year Released: " + year_released);
+        
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Student S = new Student();
-        Teacher T = new Teacher();
+        Car myCar = new Car();
+        myCar.brand = "Toyota";
+        myCar.model = "Vios";
+        myCar.color = "Purple";
+        myCar.gasoline = "Unleaded";
+        myCar.year_released = "1933";
 
-        S.setName("Mia");
-        T.setName("Jasmin");
-
-        S.showInfo();
-        T.showInfo();
-
-        S.sendMessage();
-        S.sendMessage("Good day ma'am!");
-
+        myCar.start();
+        myCar.displayInfo();
     }
 }
